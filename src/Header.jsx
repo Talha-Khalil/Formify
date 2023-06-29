54bea18a1f0eb9beda5443c036d6588ae250ec23
Talha <ka889076@gmail.com>
2023-06-29 20:11:33 +0500

    designed header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
-import './Header.css'
+import { IconButton } from '@mui/material';
+import './Header.css';
+import formIcon from './assets/images/form-icon.png';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const Header = () => {
   return (
     <div className='header_container'>
         <div className="header_info">
+            <IconButton>
             <MenuIcon/>
-
+            </IconButton>
+            <IconButton>
+            <img src={formIcon} alt="logo" />
+            </IconButton>
+            
         </div>
-        <div className="header_search"></div>
+        <div className="header_search">
+            <IconButton>
+                <SearchIcon/>
+            </IconButton>
+            <input type="text" name='search' placeholder='search here' />
+        </div>
         <div className="header_user"></div>
     </div>
   )
